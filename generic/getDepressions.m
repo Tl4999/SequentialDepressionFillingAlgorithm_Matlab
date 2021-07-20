2c5681a9146dcf0afab9a6247487db223c97a0fc
Tl4999 <62628239+Tl4999@users.noreply.github.com>
2021-07-19 22:30:42 -0400

    Update getDepressions.m

--- generic/getDepressions.m
+++ generic/getDepressions.m
@@ -1,168 +1,191 @@
-function[pits, pairs, cellIndexes, pitId, pitCell, areaCellCount, spilloverElevation, vca, volume, filledVolume, cellOverflowInto] = getDepressions(dem, flow_direction, flow_direction_parents, cellsize)
-% Generate the matrix identifying depressions in the DEM as well as their
-% parameters. 
-%
-% Inputs: 
-%
-% dem - elevation matrix
-%
-% flow_direction - flow direction matrix
-%
-% flow_direction_parents - matrix of flow direction parents
-%
-% cellsize - side length of each cell, i.e., DEM resolution (meters).
-%
-% Outputs (each of the following is an array having a length of the number of
-% depressions found in the flow direction matrix operation. Each row
-% corresponds to a different depression. Each of these parameters are 
-% needed in order to provide consistent mergers of depressions in the 
-% filling process):
-%
-% pitId - The ID number assigned to the depression.
-% pitCell - The row and column of the pit cell found in the flow direction 
-% matrix.
-%
-% areaCellCount - Total contributing area expressed as the integer count of
-% cells. Multiply by the DEM cell area to get area in square meters.
-% spilloverElevation - The elevation which sets the maximum water level of 
-% the depression after which it will overflow.
-%
-% vca - Volume to Contributing Area (VCA) ratios for each depression, also 
-% equivalent to the depth of rainfall excess the depression can retain. 
-% This value is used to sort the depressions so that they may be filled in 
-% order. Computed using volume in cubic meters and area in square meters. 
-%
-% volume - The retention volumes for each depression (m^3).
-%
-% filledVolume - The volume (m^3) filled in previous iterations.
-%
-% cellOverflowInto - The row and column of the cell in an adjacent
-% depression where water will begin to overflow into.
+import numpy as np
 
-% Preallocate variables
-disp('Creating Pit Database');
-
-pits = nan(size(dem)); % matrix identifying pits
-pit_count = sum(nansum(flow_direction == -1));
-edge_pits = int32(find(flow_direction == -2));
-pitId = int32(1 : pit_count); % id of each pit
-edgePitId = int32(-1 : -1 : (-1*length(edge_pits)-1));
-pitCell = int32(find(flow_direction == -1)); % pit bottom cell index
-edgePitCell = int32(find(flow_direction == -2));
-areaCellCount = zeros(pit_count, 1); % integer number of cells
-spilloverElevation = zeros(pit_count, 1); % meters
-vca = zeros(pit_count, 1); % volume to contributing area ratio (hours)
-volume = zeros(pit_count, 1); % (cubic meters)
-filledVolume = zeros(pit_count, 1); % (cubic meters)
-cellOverflowInto = int32(zeros(pit_count, 1)); %cell index of overflow location
-
-% Pits must first be identified in the pit matrix in order to return the
-% correct pit ID that each pit flows into (if not, many of these pits will
-% flow into yet unidentified pits that have ID 0).
-fprintf('Creating pit ID matrix...')
-fprintf('Done');
-fprintf('\n');
-fprintf(strcat('Number of depressions: ',num2str(length(pitCell))))
-fprintf('\n');
-
-% Fill out the pit ID image/matrix
-pits(~isnan(dem)) = 0;
-pits(pitCell) = pitId; % Mark the pit bottom in the pits image. This is 
-% used in recursiveMarkPits to tell what ID to mark each cell during tail
-% recursion.
-cellIndexes = cell(length(pitId), 1);
-for p = 1 : length(pitCell)
-    j = 1; % current length of cellIndexes{p}
-    i = 1; % iterator
-    chunk = 50;
-    cellIndexes{p} = nan(chunk, 1);
-    cellIndexes{p}(1) = pitCell(p);
-    while i <= j
-        parents = flow_direction_parents{cellIndexes{p}(i)};
-        pits(parents) = p;
-        k = j + length(parents);
-        if (k > chunk)
-            cellIndexes{p}(chunk+1:chunk+50) = zeros(50, 1);
-            chunk = chunk + 50;
-        end
-        cellIndexes{p}(j+1 : k) = parents;
-        j = k;
-        i = i+1;
-    end
-    cellIndexes{p}(j+1:end) = [];
-    areaCellCount(p) =j;
-end
-
-for p = 1 : length(edgePitCell)
-    j = 1; % current length of cellIndexes{p}
-    i = 1; % iterator
-    chunk = 50;
-    indexes = nan(chunk, 1);
-    indexes(1) = edgePitCell(p);
-    while i <= j
-        parents = flow_direction_parents{indexes(i)};
-        pits(parents) = -1.*p;
-        k = j + length(parents);
-        if (k > chunk)
-            indexes(chunk+1:chunk+50) = zeros(50, 1);
-            chunk = chunk + 50;
-        end
-        indexes(j+1 : k) = parents;
-        j = k;
-        i = i+1;
-    end
-end
-fprintf('\n');
-fprintf('Gathering data for each pit...');
-
-indexes = reshape(1: numel(flow_direction), size(flow_direction));
-pairs = cell(pit_count, 1);
-[numrows, numcols] = size(dem);
-parfor p = 1 : length(cellIndexes)
-    pairs{p} = zeros(length(cellIndexes{p})*8, 2);
-    l = 1;
-    for i = 1 : length(cellIndexes{p}) % walk through indicesToCheck
-        [r, c] = ind2sub(size(pits), cellIndexes{p}(i));
-        % First, loop over 3x3 neighborhood to find indicesToCheck
-        for x = -1 : 1 % loop through neighboring cells
-            for y = -1 : 1
-                if x == 0 && y ==0
-                    continue; % skip center cell of 3x3 neighborhood
-                end
-                if r+y > numrows || r+y < 1 || c+x > numcols || c+x < 1
-                    continue; % skip neighbors outside the dem range
-                end
-                if isnan(dem(r+y, c+x))
-                    continue;
-                end
-                neighbor = indexes(r+y, c+x);
-                curCell = indexes(r, c);
-                % First, check for neighbors to check next.
-                if pits(neighbor) ~= pits(curCell)
-                    pairs{p}(l, 1) = min([neighbor, curCell]); % lines are always oriented starting at lowest linear index
-                    pairs{p}(l, 2) = max([neighbor, curCell]);
-                    l = l+1;
-                end
-            end
-        end
-    end
+def getDepression(dem, flow_direction, flow_direction_parent, cellSize):
+   #Define variable
+    pits = np.array(np.empty((np.shape(dem)[0],np.shape(dem)[1])))
+    pits[:]=np.NaN
+    pit_count = np.nansum(flow_direction == -1)
+    edgepit_count = np.nansum(flow_direction == -2)
+    pitID = np.int32(np.array(range(1,pit_count + 1), dtype = int)) # pit cellID
+    #pit bottom cell index
+    #pitCell = [pitCell for (pitCell, val) in enumerate(np.ravel(flow_direction, order = 'F')) if (val == -1)]
+    pitCell = np.where(flow_direction == -1)
+    edgePitId =np.int32(np.flip(np.array(range(-edgepit_count,0), dtype = int)))
+    edgePitCell = np.where(flow_direction == -2)
+    areaCount = np.zeros((pit_count,1),dtype = int) #integer number of cell
+    spilloverElevation = np.zeros((pit_count,1),dtype = float)#meter
+    vca = np.zeros((pit_count,1),dtype = float)#volume to contributing area ratio (hours)
+    volume = np.zeros((pit_count,1),dtype = float)#cubic meters
+    filledVolume = np.zeros((pit_count,1),dtype = float)#cubic meters
+    cellOverflowInto = np.int32(np.zeros((pit_count,2),dtype = float))#cell index of overflow location
     
-    pairs{p} = pairs{p}(any(pairs{p}, 2), :); % trim zero rows
-    pairs{p} = sortrows(pairs{p}, [1,2]);
-    [val, ord] = min(max(dem(pairs{p}(:, 1:2)), [], 2)); % get minimum spillover elevation
-    spilloverElevation(p) = val;
-    cellOverflowInto(p) = pairs{p}(ord, pits(pairs{p}(ord, :)) ~= p);   
+    #Pits must be identified in the pit matrix in order to return the 
+    #correct pit ID that each pit flows into (if not, many of these pits will 
+    #flow into yet unidentifed pits that have ID 0)
     
-    % Accumulate those elevations that are less than spillover
-    lessThans = cellIndexes{p}(dem(cellIndexes{p}) <= spilloverElevation(p));
-    volume(p) = sum((spilloverElevation(p) - dem(lessThans))*cellsize*cellsize); 
-    lessThans = []; 
+    #Fill out the pit ID image/matrix
+    pits[~np.isnan(dem)] = 0 #if pits' element == NaN change the cell to 0 
+    pits[pitCell] = pitID
+    pits[edgePitCell] = edgePitId
     
-    filledVolume(p) = 0;
+    cellIndexes = [ [] for _ in range(pit_count)] #create a list within an list
 
-    vca(p) = volume(p)/((cellsize^2).*areaCellCount(p));
-    if vca(p) < 0
-        vca(p) = Inf;
-    end
-end
-end
+    for p in range(0,np.shape(pitCell)[1]):
+        pID = p + 1
+        j = 0
+        i = 0
+        chunk = 50
+        cellIndexes[p] = np.empty((chunk,2), dtype = object) 
+        cellIndexes[p][0][0] = pitCell[0][p]
+        cellIndexes[p][0][1] = pitCell[1][p]
+        
+        
+        while i <= j:
+            parent = flow_direction_parent[cellIndexes[p][i][0]][cellIndexes[p][i][1]]
+            if len(parent) != 0:
+                uparent = np.unravel_index(parent,np.shape(dem))
+                pits[uparent] = pID 
+                k = j + np.size(parent)
+                if k > (chunk-1):
+                    cellIndexes[p].append(np.empty((chunk,2),dtype= object))
+                    chunk += 50 
+                cellIndexes[p][j+1:k+1] = np.swapaxes(uparent,0,1)
+                j = k  
+            i = i + 1
+           
+        cellIndexes[p] = np.delete(cellIndexes[p],range(j+1,chunk),axis = 0) #Delete Empty or NaN array 
+        cellIndexes[p] = cellIndexes[p].astype(int)
+        areaCount[p] = j
+    
+    
+    #EDGE PITCELL
+    for p in range(0,np.shape(edgePitCell)[1]):
+        edgeID = edgePitId[p]
+        j = 0
+        i = 0
+        chunk = 50
+        edgeIndexes = np.empty((chunk,2), dtype = object) 
+        
+        edgeIndexes[0][0] = edgePitCell[0][p]
+        edgeIndexes[0][1] = edgePitCell[1][p]
+                
+        while i <= j:
+            parent = flow_direction_parent[edgeIndexes[i][0]][edgeIndexes[i][1]]
+            if len(parent) != 0:
+                uparent = np.unravel_index(parent,np.shape(dem))
+                pits[uparent] = edgeID 
+                k = j + np.size(parent)
+                if k > (chunk-1):
+                    edgeIndexes.append(np.empty((chunk,2), dtype = object))
+                    chunk += 50 
+                edgeIndexes[j+1:k+1] = np.swapaxes(uparent,0,1)
+                j = k  
+            i = i + 1   
+    
+    indexes = indexes_list = list(range(0,np.size(flow_direction)))
+    indexes = np.reshape(indexes_list,np.shape(flow_direction))
+    pairs = [ [] for _ in range(pit_count)] #create a list within an list
+    [numrows, numcols] = np.shape(dem)
+    for p in range(0,np.size(cellIndexes)):
+        pairs[p] = np.empty((np.shape(cellIndexes)[0]*8,4), dtype = int)
+        l = 0
+        for i in range(0,np.shape(cellIndexes[p])[0]): #Walk Through Indices To Check
+            [r, c] = [cellIndexes[p][i][0],cellIndexes[p][i][1]]
+           # print('cellIndexes[p][i]',cellIndexes[p][i])
+           # print('Loop', i,'r&c',[r,c])
+            for x in range(-1,2):
+                for y in range(-1,2):
+                    if x == 0 and y == 0:
+                        continue #skip the center cell
+                    if (r+y) >= (numrows-1) or (r+y)<0 or (c+x) >= (numcols-1) or (c+x)< 0:
+                        continue #skip neighbors outside of the dem range
+                    if np.isnan(dem[r+y][c+x]):
+                        continue
+                    if pits[r+y][c+x] != pits[r][c]:
+                        pairs[p][l] = [r,c,r+y,c+x]
+                        l = l + 1
+        pairs[p] = np.delete(pairs[p],range(l,np.shape(pairs[p])[0]),axis = 0)
+        
+        interior_rc =pairs[p][:,0:2]
+        exterior_rc = pairs[p][:,2:4]
+        #print('interior_rc',interior_rc,'exterior_rc',exterior_rc)
+        interiorDEM = dem[interior_rc[:,0],interior_rc[:,1]]
+        exteriorDEM = dem[exterior_rc[:,0],exterior_rc[:,1]]
+        #print('exteriorDEM',exteriorDEM)
+        #print('interiorDEM',interiorDEM)
+        
+        cmax = np.empty(np.shape(interiorDEM))
+        for i in range(0,len(interiorDEM)):
+            cmax[i] = np.max([exteriorDEM[i],interiorDEM[i]])
+        #print('MAX',cmax)
+        [val,cord] = [np.min(cmax), np.argmin(cmax)] 
+        #print([val,cord])
+        spilloverElevation[p] = val
+        cellOverflowInto[p] = exterior_rc[cord]
+        
+        #print(dem[cellIndexes[p].astype(int)])
+        lessThans = [dem[cellIndexes[p][:,0],cellIndexes[p][:,1]] <= spilloverElevation[p]] 
+        lessThans = cellIndexes[p][lessThans]
+        #print('lessThans',lessThans)
+        #print('DEM',dem[lessThans[:,0],lessThans[:,1]])
+        volume[p] = np.sum((spilloverElevation[p] - dem[lessThans[:,0],lessThans[:,1]])*cellSize*cellSize)
+        #print('loop', p, 'VOLUME',volume)
+        #print('pits',pits)
+        #print('Spill',spilloverElevation)
+        
+        filledVolume[p] = 0
+        vca[p] = volume[p]/((cellSize^2)*areaCount[p])
+        print(vca[p])
+        
+        if vca[p] < 0:
+            vca[p] = np.infty
+        
+        
+dem = np.array([[4, 7, 3, 7, 8, 8, 5, 2, 9, 8],
+ [0, 8, 2, 4, 6, 4, 7, 3, 8, 5],
+ [4, 5, 9, 9, 8, 3, 6, 4, 6, 6],
+ [6, 1, 0, 5, 7, 7, 5, 8, 8, 7],
+ [9, 9, 3, 5, 9, 7, 6, 9, 7, 2],
+ [0, 2, 8, 0, 4, 0, 1, 2, 8, 7],
+ [6, 9, 2, 9, 5, 5, 9, 1, 5, 9],
+ [4, 3, 6, 9, 1, 3, 0, 8, 1, 5],
+ [0, 1, 3, 7, 1, 9, 9, 4, 2, 8],
+ [1, 4, 4, 6, 4, 4, 8, 9, 5, 9]])
+
+flow_direction = np.array([[10., 10., 12.,  2., 13., 15.,  7., -2.,  7., 19.],
+ [-2., 10., -1., 12., 25., 25., 17.,  7., 17., -2.],
+ [10., 31., 32., 32., 25., -1., 25., 17., 17., 19.],
+ [31., 32., -1., 32., 25., 25., 25., 27., 49., 49.],
+ [50., 31., 32., 53., 53., 55., 56., 57., 49., -2.],
+ [-2., 50., 53., -1., 53., -1., 55., 56., 57., 49.],
+ [50., 51., 53., 53., 74., 55., 76., 76., 67., 78.],
+ [80., 80., 62., 74., -1., 76., -1., 76., -1., 78.],
+ [-2., 80., 81., 84., -1., 84., 76., 76., 78., 88.],
+ [80., 81., 81., 84., 84., 84., 95., 87., 88., 88.]])
+
+flow_direction_parent = np.array([[list([]), list([]), list([3]), list([]), list([]), list([]), list([]),
+  list([6, 8, 17]), list([]), list([])],
+ [list([0, 1, 11, 20]), list([]), list([2, 13]), list([4]), list([]),
+  list([5]), list([]), list([16, 18, 27, 28]), list([]), list([9, 29])],
+ [list([]), list([]), list([]), list([]), list([]),
+  list([14, 15, 24, 26, 34, 35, 36]), list([]), list([37]), list([]),
+  list([])],
+ [list([]), list([21, 30, 41]), list([22, 23, 31, 33, 42]), list([]),
+  list([]), list([]), list([]), list([]), list([]), list([])],
+ [list([]), list([]), list([]), list([]), list([]), list([]), list([]), list([]),
+  list([]), list([38, 39, 48, 59])],
+ [list([40, 51, 60]), list([61]), list([]), list([43, 44, 52, 54, 62, 63]),
+  list([]), list([45, 56, 65]), list([46, 57]), list([47, 58]), list([]),
+  list([])],
+ [list([]), list([]), list([72]), list([]), list([]), list([]), list([]),
+  list([68]), list([]), list([])],
+ [list([]), list([]), list([]), list([]), list([64, 73]), list([]),
+  list([66, 67, 75, 77, 86, 87]), list([]), list([69, 79, 88]), list([])],
+ [list([70, 71, 81, 90]), list([82, 91, 92]), list([]), list([]),
+  list([83, 85, 93, 94, 95]), list([]), list([]), list([97]),
+  list([89, 98, 99]), list([])],
+ [list([]), list([]), list([]), list([]), list([]), list([96]), list([]),
+  list([]), list([]), list([])]], dtype = object)
+
+cellSize = 10
+getDepression(dem, flow_direction, flow_direction_parent, cellSize)
